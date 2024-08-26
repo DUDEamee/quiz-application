cd61dff67da037d0dc9314fe6fb2fda5bb426fd8
DUDEamee <ameenameen8701@gmail.com>
2024-08-26 10:35:24 +0530

    updated : views

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Question,Choice,QuizAttempt
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def quiz_view(request):
+    questions = Question.objects.all()
+    if request.method == 'POST':
+        score = 0
+        for question in questions:
+            selected_choice = request.POST.get(str(question.id))
+            if selected_choice:
+                choice = Choice.objects.get(id=selected_choice)
+                if choice.is_correct:
+                    score += 1
+                QuizAttempt.objects.create(user=request,question=question,selected_choice=choice)
+        
+        return render(request, 'quiz/result.html',{'score':score})
+    return render(request ,'quiz/quiz.html',{'questions':questions})
+
+                 
+
+    
